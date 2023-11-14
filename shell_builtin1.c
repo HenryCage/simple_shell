cd3566cfc1dbe9016409f978ba922271072df16f
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-11-14 15:21:01 -0800

    All checked

--- shell_builtin1.c
+++ shell_builtin1.c
@@ -7,10 +7,11 @@
  *        constant function prototype.
  *  Return: Always 0
  */
+
 int _mihistory(info_t *note)
 {
-print_list(note->history);
-return (0);
+	print_list(note->history);
+	return (0);
 }
 
 /**
@@ -20,20 +21,24 @@ return (0);
  *
  * Return: Always 0 on success, 1 on error
  */
+
 int unset_alias(info_t *note, char *str)
 {
-char *p, c;
-int ret;
+	char *p, c;
+	int ret;
 
-p = _strchr(str, '=');
-if (!p)
-return (1);
-c = *p;
-*p = 0;
-ret = delete_node_at_index(&(note->alias),
-get_node_index(note->alias, node_starts_with(note->alias, str, -1)));
-*p = c;
-return (ret);
+	p = _strchr(str, '=');
+
+	if (!p)
+		return (1);
+
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(note->alias),
+	get_node_index(note->alias, node_starts_with(note->alias, str, -1)));
+	*p = c;
+
+	return (ret);
 }
 
 /**
@@ -43,18 +48,21 @@ return (ret);
  *
  * Return: Always 0 on success, 1 on error
  */
+
 int set_alias(info_t *note, char *str)
 {
-char *p;
+	char *p;
 
-p = _strchr(str, '=');
-if (!p)
-return (1);
-if (!*++p)
-return (unset_alias(note, str));
+	p = _strchr(str, '=');
 
-unset_alias(note, str);
-return (add_node_end(&(note->alias), str, 0) == NULL);
+	if (!p)
+		return (1);
+
+	if (!*++p)
+		return (unset_alias(note, str));
+
+	unset_alias(note, str);
+	return (add_node_end(&(note->alias), str, 0) == NULL);
 }
 
 /**
@@ -63,23 +71,28 @@ return (add_node_end(&(note->alias), str, 0) == NULL);
  *
  * Return: Always 0 on success, 1 on error
  */
+
 int print_alias(list_t *node)
 {
-char *p, *a;
-p = NULL;
-a = NULL;
+	char *p, *a;
+	p = NULL;
+	a = NULL;
 
-if (node)
-{
-p = _strchr(node->str, '=');
-for (a = node->str; a <= p; a++)
-_putchar(*a);
-_putchar('\'');
-_puts(p + 1);
-_puts("'\n");
-return (0);
-}
-return (1);
+	if (node)
+	{
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+		{
+			_putchar(*a);
+			_putchar('\'');
+			_puts(p + 1);
+			_puts("'\n");
+		}
+
+		return (0);
+	}
+
+	return (1);
 }
 
 /**
@@ -88,33 +101,37 @@ return (1);
  *          constant function prototype.
  *  Return: Always 0
  */
+
 int _mialias(info_t *note)
 {
-int i;
-char *p;
-list_t *node;
-p = NULL;
-node = NULL;
-i = 0;
+	int i;
+	char *p;
+	list_t *node;
+	p = NULL;
+	node = NULL;
+	i = 0;
 
-if (note->argc == 1)
-{
-node = note->alias;
-while (node)
-{
-print_alias(node);
-node = node->next;
-}
-return (0);
-}
-for (i = 1; note->argv[i]; i++)
-{
-p = _strchr(note->argv[i], '=');
-if (p)
-set_alias(note, note->argv[i]);
-else
-print_alias(node_starts_with(note->alias, note->argv[i], '='));
-}
+	if (note->argc == 1)
+	{
+		node = note->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
 
-return (0);
+		return (0);
+	}
+
+	for (i = 1; note->argv[i]; i++)
+	{
+		p = _strchr(note->argv[i], '=');
+		if (p)
+			set_alias(note, note->argv[i]);
+		else
+			print_alias(node_starts_with(note->alias, note->
+						argv[i], '='));
+	}
+
+	return (0);
 }
